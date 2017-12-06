d6ff3cf763d8ff7f2d97bee70c10ba70120e1c62
Divyang Soni <31805433+divyang8842@users.noreply.github.com>
2017-12-06 13:05:49 -0800

    insertion sort

---
+++ sortings/InsertionSort.java
@@ -0,0 +1,27 @@
+public class InsertionSort {
+
+	public static void main(String a[]) {
+		InsertionSort sort = new InsertionSort();
+		int[] arr1 = { 10, 34, 2, 56, 7, 67, 88, 42 };
+		int[] arr2 = sort.doInsertionSort(arr1);
+		for (int i : arr2) {
+			System.out.print(i);
+			System.out.print(", ");
+		}
+	}
+
+	public int[] doInsertionSort(int[] input) {
+
+		int temp;
+		for (int i = 1; i < input.length; i++) {
+			for (int j = i; j > 0; j--) {
+				if (input[j] < input[j - 1]) {
+					temp = input[j];
+					input[j] = input[j - 1];
+					input[j - 1] = temp;
+				}
+			}
+		}
+		return input;
+	}
+}
