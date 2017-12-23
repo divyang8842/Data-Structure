0ec815c91d1105668a337cb8a7717fd2f2ae1ce8
Divyang Soni <31805433+divyang8842@users.noreply.github.com>
2017-12-23 14:11:45 -0800

    Stack implpementation using linkedlist

---
+++ stack/Stack.java
@@ -0,0 +1,83 @@
+package stack;
+
+import singlylinkedlist.SinglyLinkedList;
+import singlylinkedlist.SinglyLinkedListNode;
+
+public class Stack<T> {
+	
+	private SinglyLinkedList<T> stackList = null;
+	
+	public Stack() {
+		stackList = new SinglyLinkedList<T>();
+	}
+	
+	public void push(T data){
+		stackList.insertAtIndex(data,0);
+	}
+	
+	public T getTop(){
+		if(stackList.isEmpty()){
+			return null;
+		}else{
+			return stackList.getNodeData(stackList.getLength()-1);
+		}
+	}
+	
+	public T pop(){
+		if(stackList.isEmpty()){
+			return null;
+		}else{
+			return stackList.delete(0);
+		}
+	}
+	
+	public boolean isEmpty(){
+		return stackList.isEmpty();
+	}
+	
+	public int size(){
+		return stackList.getLength();
+	}
+	
+	public void revertStack(Stack<T> stack){
+		
+		if(stack.e)
+		
+		T data = stack.pop();
+		
+		
+	}
+	
+	private void insertAtBottom(){
+		
+	}
+	
+	
+	public static void main(String str[]){
+		Stack<String> stack = new Stack<String>();
+		stack.push("hello");
+		stack.push("1");
+		stack.push("2");
+		stack.push("3");
+		stack.push("4");
+		stack.push("5");
+		stack.push("6");
+		stack.push("7");
+		stack.push("8");
+		stack.push("9");
+		
+		
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+	}
+	
+	
+}
