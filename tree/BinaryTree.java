b4d73c2d6d4d7084c96e59e198e4b461e9ec791e
Divyang Soni <31805433+divyang8842@users.noreply.github.com>
2017-12-26 17:23:59 -0800

    added methods in binary tree

--- tree/BinaryTree.java
+++ tree/BinaryTree.java
@@ -2,6 +2,7 @@ package tree;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class BinaryTree<T extends Number & Comparable<? super Number>> {
 	
@@ -98,13 +99,68 @@ public class BinaryTree<T extends Number & Comparable<? super Number>> {
 
 	private Comparable<? super Number> extracted(Number max) {
 		return (Comparable<? super Number>) max;
+	}
 	private T extractedT(Number max) {
 		return (T) max;
 	}
 	
 	
-	public void insertData(){
+	public int size(BinaryTreeNode<T> root){
+		int size = 0;
+		if(root!=null){
+			size = 1+size(root.getLeft()) + size(root.getRight());
+		}
+		return size;
+	}
+	
+	public int depth(BinaryTreeNode<T> root){
+		int height = 0;
+		if(root!=null){
+			height = 1+Math.max(depth(root.getLeft()) , depth(root.getRight())) ;
+		}
+		return height;
+	}
+	
+	public void levelOrderReverseTraversal(BinaryTreeNode<T> root){
+		Queue<BinaryTreeNode<T>> queue = new LinkedList<BinaryTreeNode<T>>();
+		Stack<BinaryTreeNode<T>> stack =  new Stack<BinaryTreeNode<T>>();
+		if(root==null){
+			System.out.println("Tree is empty...");
+		}
+		queue.offer(root);
+		queue.offer(null);
+		stack.push(null);
+		stack.push(root);
+		stack.push(null);
+		while(!queue.isEmpty()){
+			BinaryTreeNode<T> current = queue.poll();
+			if(current==null){
+				stack.push(null);
+				//System.out.println("");
+				queue.offer(null);
+			}else{
+				//System.out.print(current.getData()+" ");
+				stack.push(current);
+				if(current.getLeft()!=null){
+					queue.offer(current.getLeft());
+				}
+				if(current.getRight()!=null){
+					queue.offer(current.getRight());
+				}
+			}
+		}
 		
+		while(!stack.isEmpty()){
+			BinaryTreeNode<T> current =  stack.pop();
+			StringBuilder sbLevel =  new StringBuilder();
+			if(current==null){
+				System.out.println(sbLevel.toString());
+				sbLevel =  new StringBuilder();
+			}else{
+				sbLevel = new StringBuilder(String.valueOf(current.getData()) + sbLevel);
+				//System.out.print(current.getData()+" ");
+			}
+		}
 	}
 	
 	
