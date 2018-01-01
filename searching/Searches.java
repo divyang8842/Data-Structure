3b0b572d568e987f8b9e35e0558cf38fabc919c3
Divyang Soni <31805433+divyang8842@users.noreply.github.com>
2017-12-31 17:42:55 -0800

    Binary Search Algo

--- searching/Searches.java
+++ searching/Searches.java
@@ -24,4 +24,76 @@ public class Searches {
 		}
 		return -1;
 	}
+	
+	public int binarySearch(int[] dataArry,int elementToSearch, boolean bRecursive){
+		int nLeft = 0;
+		int nRight = dataArry.length-1;
+		if(bRecursive){
+			return binarySearchRecusrive(dataArry, elementToSearch, nLeft, nRight);
+		}else{
+			return binarySearchNonRecusrive(dataArry, elementToSearch, nLeft, nRight);
+		}
+			
+		
+	}
+	
+	private int binarySearchNonRecusrive(int[] dataArry, int elementToSearch,int nLeft, int nRight){
+		while(nLeft<=nRight){
+			int nMid = (nRight+nLeft)/2;
+			/*if(nMid>nLength || nMid<0){
+				return -1;
+			}*/
+			if(dataArry[nMid]==elementToSearch){
+				return nMid;
+			}else if(dataArry[nMid]>elementToSearch){
+				nRight = nMid-1;
+			}else{
+				nLeft = nMid+1;
+			}
+		}
+		
+		return -1;
+	}
+	
+	private int binarySearchRecusrive(int[] dataArry, int elementToSearch,int nLeft, int nRight){
+		
+		if(nRight>nLeft){
+			int nMiddle = (nRight+nLeft)/2;
+			int nDiff = dataArry[nMiddle] - elementToSearch;
+			
+			if(nDiff==0){
+				return nMiddle;
+			}else if(nDiff>0){
+				return binarySearchRecusrive(dataArry, elementToSearch, nLeft, nMiddle-1);
+			}else if(nDiff<0){
+				return binarySearchRecusrive(dataArry, elementToSearch, nMiddle+1, nRight);
+			}
+		
+		}
+		return -1;	
+	}
+	
+	
+	public static void main(String sytr[]){
+		Searches obj =  new Searches();
+		int[] data =  {8,5,2,9,6,3,7,4,1,0};
+		System.out.println("Non Sorted array : ");
+		System.out.println("3 is available in the array at index"+obj.searchInNonSortedArray(data, 3));
+		System.out.println("10 is available in the array at index"+obj.searchInNonSortedArray(data, 10));
+		
+		int[] data1 = {0,1,2,3,4,5,6,7,8,9,10};
+		System.out.println("Sorted array : ");
+		System.out.println("3 is available in the array at index"+obj.searchInSortedArray(data1, 3));
+		System.out.println("10 is available in the array at index"+obj.searchInSortedArray(data1, 15));
+		
+		System.out.println("binary search with sorted array");
+		System.out.println("Recursive : ");
+		System.out.println("3 is available in the array at index"+obj.binarySearch(data1, 3,true));
+		System.out.println("10 is available in the array at index"+obj.binarySearch(data1, 15,true));
+		
+		System.out.println("Non Recursive : ");
+		System.out.println("3 is available in the array at index"+obj.binarySearch(data1, 3,false));
+		System.out.println("10 is available in the array at index"+obj.binarySearch(data1, 15,false));
+		
+	}
 }
