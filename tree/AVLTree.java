c9dc283affac0a712f7a7f7f1a22242f914207a6
Divyang Soni <31805433+divyang8842@users.noreply.github.com>
2018-01-02 16:58:39 -0800

    AVL rotations

--- tree/AVLTree.java
+++ tree/AVLTree.java
@@ -24,4 +24,30 @@ public class AVLTree<T> extends BinarySearchTree<Number>{
 		}
 		return left-right;
 	}
+	
+	public AVLBinaryTreeNode<T> SingleLeftRotation(AVLBinaryTreeNode<T> root){
+		AVLBinaryTreeNode<T> left = root.getLeft();
+		root.setLeft(left.getRight());
+		left.setRight(root);
+		root.setHeight(Math.max(root.getLeft().getHeight(),root.getRight().getHeight())+1);
+		left.setHeight(Math.max(left.getLeft().getHeight(), left.getRight().getHeight())+1);
+		
+		return left;
+		
+	}
+	
+	public AVLBinaryTreeNode<T> SingleRightRotation(AVLBinaryTreeNode<T> root){
+		AVLBinaryTreeNode<T> right = root.getRight();
+		root.setRight(right.getLeft());
+		right.setLeft(root);
+		root.setHeight(Math.max(root.getLeft().getHeight(),root.getRight().getHeight())+1);
+		right.setHeight(Math.max(right.getLeft().getHeight(), right.getRight().getHeight())+1);
+		
+		return right;
+	} 
+	
+	public AVLBinaryTreeNode<T> RLRotation(AVLBinaryTreeNode<T> head){
+		head.setLeft(SingleRightRotation(head.getLeft()));
+		return SingleLeftRotation(head);
+	}
 }
