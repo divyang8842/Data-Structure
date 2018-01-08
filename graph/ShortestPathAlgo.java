03d1f1c55d85572a7659442d67a31a1ab42906e0
Divyang Soni <31805433+divyang8842@users.noreply.github.com>
2018-01-08 13:13:56 -0800

    sortest path for unweighted graph

--- graph/ShortestPathAlgo.java
+++ graph/ShortestPathAlgo.java
@@ -1,6 +1,28 @@
 package graph;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class ShortestPathAlgo {
 	//UnWeighted Graph
-	
+	public void ShortestPathUnWeightedGraph(Graph g,int start){
+		Queue<Integer> q = new LinkedList<Integer>();
+		q.offer(start);
+		for(int i=0;i<g.getVertexCount();i++){
+			g.getVertexList()[i].setWeight(-1);
+		}
+		g.getVertexList()[start].setWeight(0);
+		while(!q.isEmpty()){
+			int u = q.poll();
+			int v = g.getNonVisitedAdjVertex(u);
+			while(v!=-1){
+				if(g.getVertexList()[v].getWeight()==-1){
+					g.getVertexList()[v].setWeight(g.getVertexList()[u].getWeight()+1 );
+					g.getVertexList()[v].setParent(u);
+					q.offer(v);
+				}
+			}
+		}
+		q.clear();
+	}
 }
