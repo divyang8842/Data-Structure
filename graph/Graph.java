489a49f78ab77ce0ff0b16cf06a6b1b3d7361e88
Divyang Soni <31805433+divyang8842@users.noreply.github.com>
2018-01-08 12:40:26 -0800

    Separated Topological sorting

--- graph/Graph.java
+++ graph/Graph.java
@@ -13,19 +13,36 @@ public class Graph {
 	private boolean[][] adjacencyMAtrix;
 	private Stack<Integer>  stack =  new Stack<Integer>();
 	private Queue<Integer>  queue = new LinkedList<Integer>();
-	private int[] indegreeCount;
+
 	
 	Graph(int maxVertex){
 		this.maxVertex = maxVertex;
 		vertexList = new Vertex[maxVertex];
 		adjacencyMAtrix = new boolean[maxVertex][maxVertex];
-		indegreeCount =  new int[maxVertex];
 	}
 	
 	Graph(){
 		this(20);
 	}
 	
+	
+	
+	public int getVertexCount() {
+		return vertexCount;
+	}
+
+	public int getMaxVertex() {
+		return maxVertex;
+	}
+
+	public Vertex[] getVertexList() {
+		return vertexList;
+	}
+
+	public boolean[][] getAdjacencyMAtrix() {
+		return adjacencyMAtrix;
+	}
+
 	public void addVertex(char label){
 		if(vertexCount<maxVertex){
 			vertexList[vertexCount++] =  new Vertex(label);
@@ -102,50 +119,6 @@ public class Graph {
 		resetVisitedFlag();
 	}
 	//kahn's algorithm
-	//Topological Sort codes 
-	public void setIndegreeForNodes() {
-		for (int i = 0; i < vertexCount; i++) {
-			int inDegree = 0;
-			for (int j = 0; j < vertexCount; j++) {
-				if (adjacencyMAtrix[j][i]) {
-					inDegree++;
-				}
-			}
-			indegreeCount[i] =  inDegree;
-		}
-	}
-	
-	public void getTopologicalSort(){
-		setIndegreeForNodes();
-		Queue<Integer> topoQueue =  new LinkedList<Integer>();
-		int nSortCount = 0;
-		for(int u=0;u<vertexCount;u++){
-			if(indegreeCount[u]==0){
-				topoQueue.offer(u);
-				displayVertex(u);
-			}
-		}
-		while(!topoQueue.isEmpty()){
-			nSortCount++;
-			removeAllOutDegree(topoQueue.poll(),topoQueue);
-		}
-		
-		if(nSortCount!=vertexCount){
-			System.err.println("The grapth contains cycle.");
-		}
-	}
-	
-	public void removeAllOutDegree(int u, Queue<Integer> topoQueue){
-		for(int v=0;v<vertexCount;v++){
-			if(adjacencyMAtrix[u][v]){
-				adjacencyMAtrix[u][v] = false;
-				if(--indegreeCount[v] == 0){
-					topoQueue.offer(v);
-					displayVertex(v);
-				}
-			}
-		}
-	}
-	//Kahn's algo completed
+
 }
 
---
+++ graph/TopologicalSort.java
@@ -0,0 +1,61 @@
+package graph;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+//Kahn's algorithm
+public class TopologicalSort {
+	private int[] indegreeCount;
+	
+	private Graph g;
+	
+	TopologicalSort(Graph g){
+		this.g = g;
+		indegreeCount =  new int[g.getMaxVertex()];
+	}
+	
+	
+	public void setIndegreeForNodes() {
+		for (int i = 0; i < g.getVertexCount(); i++) {
+			int inDegree = 0;
+			for (int j = 0; j < g.getVertexCount(); j++) {
+				if (g.getAdjacencyMAtrix()[j][i]) {
+					inDegree++;
+				}
+			}
+			indegreeCount[i] =  inDegree;
+		}
+	}
+	
+	public void getTopologicalSort(){
+		setIndegreeForNodes();
+		Queue<Integer> topoQueue =  new LinkedList<Integer>();
+		int nSortCount = 0;
+		for(int u=0;u<g.getVertexCount();u++){
+			if(indegreeCount[u]==0){
+				topoQueue.offer(u);
+				g.displayVertex(u);
+			}
+		}
+		while(!topoQueue.isEmpty()){
+			nSortCount++;
+			removeAllOutDegree(topoQueue.poll(),topoQueue);
+		}
+		
+		if(nSortCount!=g.getVertexCount()){
+			System.err.println("The grapth contains cycle.");
+		}
+	}
+	
+	public void removeAllOutDegree(int u, Queue<Integer> topoQueue){
+		for(int v=0;v<g.getVertexCount();v++){
+			if(g.getAdjacencyMAtrix()[u][v]){
+				g.getAdjacencyMAtrix()[u][v] = false;
+				if(--indegreeCount[v] == 0){
+					topoQueue.offer(v);
+					g.displayVertex(v);
+				}
+			}
+		}
+	}
+}
