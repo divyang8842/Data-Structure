b8c967e931e3f2cd5fdf74cc0f9c6167713bf436
Divyang Soni <31805433+divyang8842@users.noreply.github.com>
2018-01-09 12:02:02 -0800

    Generic Sorting : Bubble Sort

---
+++ sortings/generic/BubbleSort.java
@@ -0,0 +1,46 @@
+package sortings.generic;
+
+public class BubbleSort<T extends Comparable<T>> {
+	
+	//ascending
+	public T[] doSort(T[] dataArry){
+		int nLength =  dataArry.length;
+		boolean bSwapped = true;
+		for(int nSorted=nLength; nSorted>0 && bSwapped ;nSorted--){
+				bSwapped = false;
+				System.out.println("Inetarion "+(nLength-nSorted));
+				for(int nCurrent=0;nCurrent<nSorted-1;nCurrent++){
+					if(dataArry[nCurrent].compareTo(dataArry[nCurrent+1])>0){
+						bSwapped = true;
+						T temp =  dataArry[nCurrent];
+						dataArry[nCurrent] =  dataArry[nCurrent+1];
+						dataArry[nCurrent+1] =  temp;
+					}
+				}
+		}
+		return dataArry;
+	}
+	
+	public void printArry(T[] dataArry){
+		int nLength =  dataArry.length;
+		for(int i=0;i<nLength;i++){
+			System.out.print(dataArry[i]+" ");
+		}
+		System.out.println(" ");
+	}
+	
+	public static void main(String str[]){
+		BubbleSort<Integer> objSort =  new BubbleSort<Integer>();
+		Integer[] data = {0,1,2,3,4,5,6,7,8,9,10};
+		data = objSort.doSort(data);
+		objSort.printArry(data);
+		
+		
+		Integer[] data1 = {10,9,8,7,6,5,4,3,2,1,0};
+		data1 = objSort.doSort(data1);
+		objSort.printArry(data1);
+	}
+	
+	
+	
+}
