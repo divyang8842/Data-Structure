a9dc937da7adf3a6205f2cbcd4a89e0cfd5e3c52
Divyang Soni <31805433+divyang8842@users.noreply.github.com>
2018-01-08 17:19:26 -0800

    DAG Shprtest Path algo

--- graph/ShortestPathAlgo.java
+++ graph/ShortestPathAlgo.java
@@ -1,6 +1,7 @@
 package graph;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 import tree.HeapTree;
@@ -28,29 +29,62 @@ public class ShortestPathAlgo {
 		q.clear();
 	}
 	
-	public void shortestPathForWeightedGraph(Graph g,int s){//dijkstra
-		HeapTree<Vertex> minPriqueue =  new HeapTree(HeapTree.MIN_HEAP,g.getVertexCount());
-		int u,v;
-		
+	public void shortestPathForWeightedGraph(Graph g, int s) {// dijkstra
+		HeapTree<Vertex> minPriqueue = new HeapTree(HeapTree.MIN_HEAP,g.getVertexCount());
+		int u, v;
+
 		try {
+			for (int i = 0; i < g.getVertexCount(); i++) {
+				g.getVertexList()[i].setWeight(-1);
+			}
+			g.getVertexList()[s].setWeight(0);
 			minPriqueue.insert(g.getVertexList()[s]);
-			while(!minPriqueue.isEmpty()){
-				u =  minPriqueue.getFirst().getIndex();
-				v = g.getAdjVertex(u);
-				while(v!=-1){
-					
-					if(relaxTheEdge(u, v,g)){
-						if(!g.getVertexList()[v].isVisited()){
-							minPriqueue.insert(g.getVertexList()[v]);
-							g.getVertexList()[v].setVisited(true);
-						}else{
-							minPriqueue.updatePriority(g.getVertexList()[v]);
+			int vertexCount = g.getVertexCount();
+			while (!minPriqueue.isEmpty()) {
+				u = minPriqueue.getFirst().getIndex();
+				v = 0;
+
+				while (v < vertexCount) {
+					if (g.getAdjacencyMAtrix()[u][v] == 1 && u != v) {
+
+						if (relaxTheEdge(u, v, g)) {
+							if (!g.getVertexList()[v].isVisited()) {
+								minPriqueue.insert(g.getVertexList()[v]);
+								g.getVertexList()[v].setVisited(true);
+							} else {
+								minPriqueue.updatePriority(g.getVertexList()[v]);
+							}
 						}
 					}
+					v++;
 				}
+
 			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+	
+	public void ShortestPathWeightedAlgo(Graph g){//DAG
+		List<Integer> list = new TopologicalSort(g).getTopologicalSort();
+		int length = list.size();
+		int curIndex=0;
+		for(int i=0;i<g.getVertexCount();i++){
+			g.getVertexList()[i].setWeight(-1);
+		}
+		g.getVertexList()[list.get(curIndex)].setWeight(0);
+		int vertexCount = g.getVertexCount();
+		while(curIndex<length){
+			int u = list.get(curIndex);
+			int v = 0;
+
+			while (v < vertexCount) {
+				if (g.getAdjacencyMAtrix()[u][v] == 1 && u != v) {
+					relaxTheEdge(u, v, g);
+				}
+			}
+			curIndex++;
 		}
 	}
 	
--- graph/TopologicalSort.java
+++ graph/TopologicalSort.java
@@ -1,6 +1,7 @@
 package graph;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 //Kahn's algorithm
@@ -27,33 +28,38 @@ public class TopologicalSort {
 		}
 	}
 	
-	public void getTopologicalSort(){
+	public List<Integer> getTopologicalSort(){
 		setIndegreeForNodes();
+		List<Integer> topologicalSort  = new LinkedList<Integer>();
 		Queue<Integer> topoQueue =  new LinkedList<Integer>();
 		int nSortCount = 0;
 		for(int u=0;u<g.getVertexCount();u++){
 			if(indegreeCount[u]==0){
 				topoQueue.offer(u);
+				topologicalSort.add(g.getVertexList()[u].getIndex());
 				g.displayVertex(u);
 			}
 		}
 		while(!topoQueue.isEmpty()){
 			nSortCount++;
-			removeAllOutDegree(topoQueue.poll(),topoQueue);
+			removeAllOutDegree(topoQueue.poll(),topoQueue,topologicalSort);
 		}
 		
 		if(nSortCount!=g.getVertexCount()){
 			System.err.println("The grapth contains cycle.");
 		}
+		
+		return topologicalSort;
 	}
 	
-	public void removeAllOutDegree(int u, Queue<Integer> topoQueue){
+	public void removeAllOutDegree(int u, Queue<Integer> topoQueue, List<Integer> topologicalSort){
 		for(int v=0;v<g.getVertexCount();v++){
 			if(g.getAdjacencyMAtrix()[u][v]==1){
 				g.getAdjacencyMAtrix()[u][v] = 0;
 				if(--indegreeCount[v] == 0){
 					topoQueue.offer(v);
 					g.displayVertex(v);
+					topologicalSort.add(g.getVertexList()[v].getIndex());
 				}
 			}
 		}
