d9fe20a62c6f6e37a3d72b538ec37d3eefce2662
Divyang Soni <31805433+divyang8842@users.noreply.github.com>
2018-01-10 15:32:37 -0800

    Random methods added for practice

--- random/Practice.java
+++ random/Practice.java
@@ -1,5 +1,8 @@
 package random;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import sortings.HeapSort;
 
 public class Practice {
@@ -13,7 +16,6 @@ public class Practice {
 				}else{
 					sum+=dataArry[i][j]+dataArry[i][j+1];
 				}
-				
 			}
 		}
 		return sum;
@@ -46,21 +48,40 @@ public class Practice {
 		return false;
 	}
 	
+	public boolean isSumAvailableInUnSortedArray(int[] arry,int sum){
+		int nLeft = 0;
+		int nRight = arry.length;
+		Set<Integer> complement = new HashSet<Integer>();
+		
+		while(nRight>nLeft){
+			if(complement.contains(arry[nLeft])){
+				return true;
+			}else{
+				complement.add(sum - arry[nLeft]);
+			}
+			nLeft++;
+		}
+		complement.clear();
+		complement =null;
+	return false;
+}
 	public static void main(String str[]){
 		Practice obj = new Practice();
 		int[][]data = {{1,2,3,13},{4,5,6,14},{7,8,9,15},{10,11,12,16}};
-		System.out.println(obj.sumOfAllElementIn2DArray(data, 4, 4));
-		
+		System.out.println(" Sum of all element is "+obj.sumOfAllElementIn2DArray(data, 4, 4));
 		
 		int data0[] = {3,7,4,6,8,5,3,5,79,5,3,5,7,9,1,3,8,0};
-		System.out.println(obj.foundIfRepeatedNumber(data0));
+		System.out.println("repeated number is available : "+obj.foundIfRepeatedNumber(data0));
 		
 		int data1[] = {1,2,3,4,5,6,7,8,9,10,11,12,13};
-		System.out.println(obj.foundIfRepeatedNumber(data1));
+		System.out.println("repeated number is available : "+obj.foundIfRepeatedNumber(data1));
 		
+		System.out.println("is Sum Available in sorted array : "+obj.isSumAvailableInSortedArray(data1, 5000));
 		
-		System.out.println(obj.isSumAvailableInSortedArray(data1, 5000));
+		System.out.println("is Sum Available in sorted array : "+obj.isSumAvailableInSortedArray(data1, 10));
 		
-		System.out.println(obj.isSumAvailableInSortedArray(data1, 10));
+		System.out.println("is Sum Available in unsorted array : "+obj.isSumAvailableInUnSortedArray(data0, 5000));
+		
+		System.out.println("is Sum Available in unsorted array : "+obj.isSumAvailableInUnSortedArray(data0, 10));
 	}
 }
