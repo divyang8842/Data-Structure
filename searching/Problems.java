08162455cd9625390c91b6f31a07bcd2219cabdd
Divyang Soni <31805433+divyang8842@users.noreply.github.com>
2018-01-11 13:26:52 -0800

    Random problems in search

--- searching/Problems.java
+++ searching/Problems.java
@@ -63,15 +63,50 @@ public class Problems extends Searches{
 		obj.sort(dataArry);
 		int j=0;
 		int i = nLength-1;
-		while(i>j){
+		int sum = Integer.MAX_VALUE ,element1=0,element2=0;
+		while(i>j && sum!=0){
+			int currentSum = dataArry[i]+dataArry[j];
 			
+			if(Math.abs(currentSum)< Math.abs(sum) || currentSum==0){
+				sum =  currentSum;
+				element1 = dataArry[j];
+				element2 = dataArry[i];
+			}
+			if(currentSum<0){
+				j++;
+			}else if(currentSum>0){
+				i--;
+			}
 		}
 		
-		
-		
+		System.out.println("Sum close to 0 is "+sum +" with elements "+element1+" and "+element2);
+	}
+	
+	public boolean is3ElementsAvailableWithSumK(int[] dataArry,int k){
+		QuickSort sort = new QuickSort();
+		sort.sort(dataArry);
+		int nLength =  dataArry.length;
+		for(int i=0;i<nLength-2;i++){
+			int sum = k- dataArry[i];
+			int nLeft = i+1;
+			int nRight = nLength-1;
+			while(nRight>nLeft){
+				int nCurSum = dataArry[nLeft] + dataArry[nRight];
+ 				if(nCurSum == sum){
+ 					System.out.println("Elements are "+dataArry[i]+","+dataArry[nLeft]+","+dataArry[nRight]);
+ 					return true;
+ 				}else if(nCurSum>sum){
+ 					nRight--;
+ 				}else{
+ 					nLeft++;
+ 				}
+			}
+		}
+		return false;
 	}
 	
 	
+
 	public static void main(String str[]){
 		Problems obj = new Problems();
 		int[] data = {1,1,1,1,1,2,3,4,5,6,7,8,9,10,11,11,11,11,11,11,11,11,11,11,11,11};
@@ -83,5 +118,16 @@ public class Problems extends Searches{
 		
 		int[] dataSqr = {4,3,5,6};
 		System.out.println(obj.findSumOfSquareInArray(dataSqr));
+		
+		
+		int[] dataSum = {4,-1,-2,9,-3,-4,0,5,6,7,8};
+		obj.find2ElementWithSumNearTo0(dataSum);
+		
+		
+		int[] data3Sum = {1,2,3,4,0,6,9,5,15};
+		System.out.println("is 3 elements available having sum 29 :"+obj.is3ElementsAvailableWithSumK(data3Sum, 29));
+		
+		
+		
 	}
 }
