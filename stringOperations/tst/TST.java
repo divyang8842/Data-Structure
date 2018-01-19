de4b37de6f845f6a58cb5bd94f3d1199a9fdc13c
Divyang Soni <31805433+divyang8842@users.noreply.github.com>
2018-01-18 21:55:35 -0800

    TST insertion implementation

---
+++ stringOperations/tst/TST.java
@@ -0,0 +1,35 @@
+package stringOperations.tst;
+
+public class TST {
+	private TSTNode root;
+	
+	public TSTNode InsertInTST(TSTNode root,String strWord, int nPosition){
+		if(root==null){
+			if(strWord.length()<=nPosition){
+				return root;
+			}
+			root = new TSTNode();
+			root.setcData(strWord.charAt(nPosition));
+			root.setleft(null);
+			root.setright(null);
+			if(nPosition==strWord.length()-1){
+				root.setB_is_End_Of_String(true);
+				return root;
+			}else{
+				root.setEq(InsertInTST(root.getEq(), strWord, nPosition+1));
+			}
+		}else if(strWord.charAt(nPosition)>root.getcData()){
+			root.setright(InsertInTST(root.getright(), strWord, nPosition));
+		}else if(strWord.charAt(nPosition)==root.getcData()){
+			if(strWord.length()<=nPosition){
+				root.setEq(InsertInTST(root.getEq(), strWord, nPosition));
+			}else{
+				root.setB_is_End_Of_String(true);
+			}
+			
+		}else{
+			root.setleft(InsertInTST(root.getEq(), strWord, nPosition));
+		}
+		return root;
+	}
+}
