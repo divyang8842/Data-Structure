18e4181fbaeff7bc428104347986dcc13923982d
Divyang Soni <31805433+divyang8842@users.noreply.github.com>
2018-01-19 02:13:24 -0800

    Find Anagram or not

--- stringOperations/Problems.java
+++ stringOperations/Problems.java
@@ -1,5 +1,7 @@
 package stringOperations;
 
+import java.util.LinkedList;
+
 public class Problems {
 	//O(n)
 	public String reverseString(String strData){
@@ -13,4 +15,31 @@ public class Problems {
 		}
 		return strData;
 	}
+	
+
+	public boolean findIfStringsAreAnagram(String str1,String str2){
+		int nLength1 = str1.length();
+		int nLength2 = str2.length();
+		int xoredNumber = 0;
+		if(nLength1==nLength2){
+			while(nLength1>0){
+				nLength1--;
+				
+				xoredNumber^=   ((int)str1.charAt(nLength1) ^ (int)str2.charAt(nLength1));
+				System.out.println(xoredNumber);
+			}
+			return xoredNumber==0;
+		}
+		return false;
+	}
+	
+	public static void main(String[] str){
+		Problems obj = new Problems();
+		System.out.println("123456789 & 147258369 are anagram : "+obj.findIfStringsAreAnagram("123456789", "142258369"));
+	}
+	
+	
+
+
+
 }
