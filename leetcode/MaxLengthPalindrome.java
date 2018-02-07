1d3e1b924f97195a791b67bbc907af7d80ca71f7
Divyang Soni <31805433+divyang8842@users.noreply.github.com>
2018-02-07 15:25:35 -0800

    Max Lngth Palindrome

---
+++ leetcode/MaxLengthPalindrome.java
@@ -0,0 +1,44 @@
+package leetcode;
+import java.util.HashMap;
+public class MaxLengthPalindrome {
+
+	    public String longestPalindrome(String s) {
+	        
+	        int nLength= s.length();
+	        int nLeft = 0;
+	        int nRight = 0;
+	        int nStart = 0;
+	        int nMax = 0;
+	        for(int i=0;i<nLength;i++){
+	            
+	            nLeft = i-1;
+	            nRight = i+1;;
+	            int nMaxLength = 1;
+	            while(nLeft>=0 && nRight<nLength && (s.charAt(nLeft) == s.charAt(nRight))){
+	                nLeft--;
+	                nRight++;
+	                nMaxLength+=2;
+	            }
+	            if(nMaxLength>nMax){
+	                nMax = nMaxLength;
+	                nStart = nLeft+1;
+	            }
+	            
+	            
+	            nLeft = i;
+	            nRight = i+1;;
+	            nMaxLength = 0;
+	            while(nLeft>=0 && nRight<nLength && (s.charAt(nLeft) == s.charAt(nRight))){
+	                nLeft--;
+	                nRight++;
+	                nMaxLength+=2;
+	            }
+	            if(nMaxLength>nMax){
+	                nMax = nMaxLength;
+	                nStart = nLeft+1;
+	            }
+	        }
+	            
+	        return s.substring(nStart,nStart+nMax);
+	    }
+}
