e914007d87b254285edbb12512d8ad44026ef5bc
Divyang Soni <31805433+divyang8842@users.noreply.github.com>
2018-02-18 11:59:32 -0800

    Code with more efficiency

--- random/Test.java
+++ random/Test.java
@@ -7,86 +7,78 @@ public class Test {
 	 */
 	public static void main(String[] args)
 	{
-		String []arr={"aqc"};
-		System.out.println(strengthenPasswords(arr));
+		String []arr={"a12345678910x5cnExTcapital1","aNExtCapital6x12345678912","neXtCapital9nextcapital","AneXtCapital9nextcapitalZ"};
+		strengthenPasswords(arr);
 	}
-	
-	    static String[] strengthenPasswords(String[] passwords) {
-	        int slen = passwords.length;
-	        String[] output = new String[slen];
-	       
-	        for(int n=0;n<slen;n++)
-	        {
-	            int newValue = 0;
-	            int len=passwords[n].length();
-	            char[] chars = new char[len+1];
-	            int j=0;
-	            for(int i=0;i<len;i++)
-	            {
-	                if(passwords[n].charAt(i)=='s' || passwords[n].charAt(i)=='S'){
-	                    chars[j]='5';
-	                }else{
-	                	chars[j] = passwords[n].charAt(i);
-	                }
-	                if(len>1 && len%2==1 && i==(len-1)/2)
-		            {
-	                	if(Character.isDigit(chars[i])&& Integer.parseInt(String.valueOf(passwords[n].charAt(i))) > 4){
-		                	chars[++j] = 0;
-		                }
-		            }	
-	                j++;
-	            }
-	            
-	            if(len>1 && len%2==1 && Character.isDigit(chars[(len-1)/2]))
-	            {
-	                newValue = Integer.parseInt(String.valueOf(chars[(len-1)/2]))*2;
-	                if(newValue>9){
-	                	chars[(len-1)/2] = Character.forDigit(newValue/10, 10);
-	                	chars[(len-1)/2 +1] = Character.forDigit(newValue%10, 10);
-	                }else{
-	                    chars[(len-1)/2] = Character.forDigit(newValue, 10);
-	                }
-	            }
-	            
-	            
-	            
-	            if(len%2==0)
-	            {
-	                char c=chars[0];
-	                
-	                chars[0]=chars[len-1];
-	                
-	                if(Character.isUpperCase(chars[0]))
-	                   chars[0]=Character.toLowerCase(chars[0]);
-	                else if(Character.isLowerCase(chars[0]))
-	                   chars[0]=Character.toUpperCase(chars[0]);
-	                
-	                chars[len-1]=c;
-	                
-	                if(Character.isUpperCase(chars[len-1]))
-	                   chars[len-1]=Character.toLowerCase(chars[len-1]);
-	                else if(Character.isLowerCase(chars[len-1]))
-	                   chars[len-1]=Character.toUpperCase(chars[len-1]);
-	            }
-	            String str = String.valueOf(chars);
-	            output[n]=str.trim();
-	            if(str.toLowerCase().contains("nextcapital"))
-	            {
-	                int startIndex = str.toLowerCase().indexOf("next");
-	                String result = str.substring(0,startIndex);
-	                result += new StringBuilder(str.substring(startIndex,startIndex+4)).reverse().toString();
-	                result += str.substring(startIndex+4,len);
-	                output[n]=result.trim();
-	            }
-	            
-	        }
-	        
-	        for(int i=0;i<output.length;i++)
-	        {
-	        	System.out.println(output[i]);
-	        }
-	        
-	        return output;
-	    }
-	    
+	static String[] strengthenPasswords(String[] passwords) {
+		int nLength = passwords.length;
+        
+        for(int i=0;i<nLength;i++)
+        {
+        	int nPasLength = passwords[i].length();
+        	boolean isMiddleDigit =  false;
+        	boolean bEvenLength = false;
+        	int nMiddle = Character.getNumericValue(passwords[i].charAt(nPasLength/2));
+        	if(nPasLength%2 == 1){
+        		if(Character.isDigit(passwords[i].charAt(nPasLength/2))){
+        			isMiddleDigit = true;
+        			if(nMiddle >4){
+        				nPasLength++;
+        				bEvenLength = true;
+        			}
+        		}
+        	}else{
+        		bEvenLength = true;
+        	}
+        	char[] cPassword = new char[nPasLength];
+        	int nCharIndex = 0;
+        	for(int j=0;j<nPasLength;j++,nCharIndex++){
+        		if(isMiddleDigit && nPasLength/2 - 1 == j){
+        			nMiddle = nMiddle*2;
+        			if(nMiddle>9){
+        				cPassword[j++] = '1';
+        				cPassword[j] = Character.forDigit(nMiddle%10, 10);
+        			}else{
+        				cPassword[j] = Character.forDigit(nMiddle, 10);
+        			}
+        		}else{
+        			cPassword[j] = passwords[i].charAt(nCharIndex);
+        			if(Character.toLowerCase(cPassword[j]) == 's'){
+        				cPassword[j] = '5';
+        			}
+        		}
+        	}
+        	if(bEvenLength){
+        		char cTemp  = cPassword[nPasLength-1];
+        		cPassword[nPasLength-1] =  cPassword[0];
+        		cPassword[0] = cTemp;
+        		if(Character.isUpperCase(cPassword[nPasLength-1])){
+        			cPassword[nPasLength-1] =  Character.toLowerCase(cPassword[nPasLength-1]);
+        		}else{
+        			cPassword[nPasLength-1] = Character.toUpperCase(cPassword[nPasLength-1]);
+        		}
+        		
+        		if(Character.isUpperCase(cPassword[0])){
+        			cPassword[0] =  Character.toLowerCase(cPassword[0]);
+        		}else{
+        			cPassword[0] = Character.toUpperCase(cPassword[0]);
+        		}
+        	}
+        	int nIndex = passwords[i].toLowerCase().indexOf("nextcapital"); 
+        	if(nIndex>0 && nIndex< nPasLength - 11){
+        		if(isMiddleDigit && bEvenLength && nIndex > nPasLength/2){
+        			nIndex++;
+        		}
+        		char cTemp  = cPassword[nIndex];
+        		cPassword[nIndex] = cPassword[nIndex+3];
+        		cPassword[nIndex+3] = cTemp;
+        		cTemp = cPassword[nIndex+1];
+        		cPassword[nIndex+1] = cPassword[nIndex+2];
+        		cPassword[nIndex+2] = cTemp;
+        	}
+        	passwords[i] = String.valueOf(cPassword);
+        	System.out.println(passwords[i]);
+        }
+        return passwords;
+	}
 }
