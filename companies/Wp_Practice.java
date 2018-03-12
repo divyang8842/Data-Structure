06781497b5352196b09f0a5f16d836fcbb63ca9f
Divyang Soni <divyang8842@gmail.com>
2018-03-12 13:59:00 -0700

    Valid Number

--- companies/Wp_Practice.java
+++ companies/Wp_Practice.java
@@ -48,13 +48,81 @@ public class Wp_Practice {
 	}
 	//## End
 	
+	//## check if string is valid number or not
+	public boolean isValidNumber(String strNumber) {
+		
+		//if input is null
+		if(strNumber==null) {
+			return false;
+		}
+		//removing extra spaces from the input
+		strNumber =  strNumber.trim();
+		//getting length of the input
+		int nLength = strNumber.length();
+		//if input is empty string
+		if(nLength==0) {
+			return false;
+		}
+		//if input has only one character and its not a digit
+		if(nLength==1 && !Character.isDigit(strNumber.charAt(0))) {
+			return false;
+		}
+		
+		//if first character of number is not a digit, '+', '-', '.'
+		if(!Character.isDigit(strNumber.charAt(0)) && strNumber.charAt(0)!='+' && strNumber.charAt(0)!='-' && strNumber.charAt(0)!='.'){
+			return false;
+		}
+		
+		//check if dot or e is coming for 1time and also . comes before e
+		//boolean to check Dot
+		boolean isDot = false;
+		//boolean to check e
+		boolean isE = false;
+		for(int i=1;i<nLength;i++) {
+			if(strNumber.charAt(i)=='.') {
+				//valid number can not have dot for 2 times
+				// also dot can not appear after e
+				if(isDot || isE) {
+					return false;
+				}else {
+					isDot = true;
+				}
+			}else if(strNumber.charAt(i)=='e') {
+				//valid number can not have e two times
+				if(isE) {
+					return false;
+				}
+				i++;
+				
+				//e can not be last character of number
+				if(i==nLength) {
+					return false;
+				}
+				
+				//number after 
+				if(strNumber.charAt(i)!='+' && strNumber.charAt(i)!='-' && !Character.isDigit(strNumber.charAt(i))) {
+					return false;
+				}
+				isE = true;
+				
+			}else if(!Character.isDigit(strNumber.charAt(i))) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	//## End
+	
 	
 	public static void main(String[] str) {
 		
-		 int G[][] = {{0, 1, 0, 1},
-                 {1, 0, 1, 0},
-                 {0, 1, 0, 1},
-                 {1, 0, 1, 0}};
+		 int G[][] = {
+				 	 {0, 1, 0, 1},
+				 	 {1, 0, 1, 0},
+				 	 {0, 1, 0, 1},
+				 	 {1, 0, 1, 0}
+				 	 };
 		 Wp_Practice obj = new Wp_Practice();
 		 if(obj.isBipartite(G)) {
 			 System.out.println("True");
@@ -62,5 +130,10 @@ public class Wp_Practice {
 			 System.out.println("False");
 		 }
 		 
+		 if(obj.isValidNumber("1.0e5e9")) {
+			 System.out.println("Valid Number.");
+		 }else {
+			 System.out.println("Invalid Number.");
+		 }
 	}
 }
